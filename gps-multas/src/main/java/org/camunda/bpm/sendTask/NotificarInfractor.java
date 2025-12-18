8c9a1a1e35c2db011fe77d16a3dd59bbf4684cab
pamverper <pamverper@alum.us.es>
2025-12-18 23:43:41 +0100

    1

--- gps-multas/src/main/java/org/camunda/bpm/sendTask/NotificarInfractor.java
+++ gps-multas/src/main/java/org/camunda/bpm/sendTask/NotificarInfractor.java
@@ -1,5 +1,57 @@
 package org.camunda.bpm.sendTask;
 
-public class NotificarInfractor {
+import java.util.logging.Logger;
+
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+
+
+public class NotificarInfractor implements ExternalTaskHandler{
+	
+	//Handler correo
+    private static final Logger LOGGER = Logger.getLogger(NotificarInfractor.class.getName());
+    
+    private MailGenerico mailWorker;
+
+    public NotificarInfractor() {
+        this.mailWorker = new MailGenerico();
+    }
+
+    
+    //DATOS CORREO:
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+
+        // CONFIGURA los valores específicos para este correo
+    	String destinatario = "lauritagaro14@gmail.com";
+        Integer idInfractor = Integer.parseInt(externalTask.getVariable("id_infractor"));
+        Integer idInfraccion = Integer.parseInt(externalTask.getVariable("id_infraccion"));
+
+        // Construye el mensaje específico de confirmación de pagoS
+        String asunto = "Multas - Infraccion registrada en el sistema";
+        String mensaje = "\nSe ha registrado una infraccion en el sistema\n\n" +
+                         "\n - Id infractor: " + idInfractor +
+                         "\n - Id de la infraccion: " + idInfraccion + 
+                         "\n\nEste mensaje ha sido generado automaticamente.";
+
+        LOGGER.info("CONTENIDO DEL MENSAJE A ENVIAR: " + mensaje);
+
+        try {
+            mailWorker.enviarCorreo(destinatario, asunto, mensaje);
+            LOGGER.info(">> El correo electrónico ha sido enviado");
+            externalTaskService.complete(externalTask);
+        } catch (Exception e) {
+            LOGGER.warning(">> Error al enviar correo: " + e.getMessage());
+            externalTaskService.handleFailure(externalTask,
+                "Error al enviar correo",
+                e.getMessage(),
+                0,
+                1000);
+        }
+
+        System.out.println(" >> ----------\n");
+    }
+
 
 }
